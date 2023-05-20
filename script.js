37b5877ca235c30ede427c9b4ae8fe8c0efda968
MelvinTurciosCs <89433567+MelvinTurciosCs@users.noreply.github.com>
2023-05-20 01:28:21 -0500

    Added the files for the Orange Belt Project

---
+++ script.js
@@ -0,0 +1,6 @@
+document.getElementById("add-to-cart").addEventListener("click", addToCart);
+
+function addToCart() {
+  // Add your logic here for adding the product to the cart
+  console.log("Product added to cart");
+}
